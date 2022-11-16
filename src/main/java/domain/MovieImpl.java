88405762c1e5564b83a469680cb240fe2dc5b047
M V K <krishna.mugala9@gmail.com>
2022-11-16 14:31:56 +0530

    called the methods using beans in impl class and printed on the console

--- src/main/java/domain/MovieImpl.java
+++ src/main/java/domain/MovieImpl.java
@@ -5,16 +5,26 @@
  *
  */
 
-
 package domain;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MovieImpl {
-
+    /**
+     * The main function creates an ApplicationContext object, which is an interface that represents the Spring IoC
+     * container. The ApplicationContext object is created by passing the MovieConfig class to the
+     * AnnotationConfigApplicationContext class. The ApplicationContext object is then used to get the Movie object
+     */
     public static void main(String[] args) {
-
+        ApplicationContext context = new AnnotationConfigApplicationContext(MovieConfig.class);
+        Movie movie1 = (Movie) context.getBean("movieBean1");
+        System.out.println(movie1);
+        Movie movie2 = (Movie) context.getBean("movieBean1");
+        System.out.println(movie2);
+        Movie movie3 = (Movie) context.getBean("movieBean1");
+        System.out.println(movie3);
+        Movie movie4 = (Movie) context.getBean("movieBean1");
+        System.out.println(movie4);
     }
-
 }
