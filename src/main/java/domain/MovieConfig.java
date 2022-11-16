626eba55aaa716fd0eb2ef5263b1f248d44eca46
M V K <krishna.mugala9@gmail.com>
2022-11-16 14:19:08 +0530

    added beans in tha config class

--- src/main/java/domain/MovieConfig.java
+++ src/main/java/domain/MovieConfig.java
@@ -8,5 +8,26 @@
 
 package domain;
 
+import org.springframework.context.annotation.Bean;
+
 public class MovieConfig {
+    @Bean("movieBean1")
+    public Movie getMovieBean1() {
+        return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
+    }
+
+    @Bean("movieBean2")
+    public Movie getMovieBean2() {
+        return new Movie("ArjunReddy", "Vijay", "shalini", 8, "asdkjfo");
+    }
+
+    @Bean("movieBean3")
+    public Movie getMovieBean3() {
+        return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
+    }
+
+    @Bean("movieBean4")
+    public Movie getMovieBean4() {
+        return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
+    }
 }
