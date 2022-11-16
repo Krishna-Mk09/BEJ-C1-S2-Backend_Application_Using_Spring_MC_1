77b7dd0950cda457d8da06c7791b22c3bb6a9d93
M V K <krishna.mugala9@gmail.com>
2022-11-16 14:06:55 +0530

    created getter and setters methods in the movie class

--- src/main/java/domain/Movie.java
+++ src/main/java/domain/Movie.java
@@ -25,4 +25,44 @@ public class Movie {
         this.movieRating = movieRating;
         this.movieType = movieType;
     }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieActor() {
+        return movieActor;
+    }
+
+    public void setMovieActor(String movieActor) {
+        this.movieActor = movieActor;
+    }
+
+    public String getMovieActress() {
+        return movieActress;
+    }
+
+    public void setMovieActress(String movieActress) {
+        this.movieActress = movieActress;
+    }
+
+    public int getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(int movieRating) {
+        this.movieRating = movieRating;
+    }
+
+    public String getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(String movieType) {
+        this.movieType = movieType;
+    }
 }
