6fbbad5ed2c7f9879bf479380de0e1fe776a8b04
M V K <krishna.mugala9@gmail.com>
2022-11-16 14:21:48 +0530

    added java docs to config class

--- src/main/java/domain/MovieConfig.java
+++ src/main/java/domain/MovieConfig.java
@@ -11,23 +11,46 @@ package domain;
 import org.springframework.context.annotation.Bean;
 
 public class MovieConfig {
-    @Bean("movieBean1")
-    public Movie getMovieBean1() {
-        return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
-    }
-
-    @Bean("movieBean2")
-    public Movie getMovieBean2() {
-        return new Movie("ArjunReddy", "Vijay", "shalini", 8, "asdkjfo");
-    }
-
+    /**
+     * The @Bean annotation tells Spring that the method will return an object that should be registered as a bean in the
+     * Spring application context
+     *
+     * @return A Movie object with the given parameters.
+     */
     @Bean("movieBean3")
     public Movie getMovieBean3() {
         return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
     }
 
+    /**
+     * The @Bean annotation tells Spring that the method will return an object that should be registered as a bean in the
+     * Spring application context
+     *
+     * @return A Movie object with the given parameters.
+     */
     @Bean("movieBean4")
     public Movie getMovieBean4() {
         return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
     }
+
+    /**
+     * @return A Movie object
+     * @Bean("movieBean1") is a function that returns a new Movie object with the given parameters
+     */
+    @Bean("movieBean1")
+    public Movie getMovieBean1() {
+        return new Movie("Titanic", "Jack", "Rose", 7, "asdkjfo");
+    }
+
+
+    /**
+     * @return Movie Object
+     * @Bean annotation is used to create a bean in the Spring container
+     */
+    @Bean("movieBean2")
+    public Movie getMovieBean2() {
+        return new Movie("ArjunReddy", "Vijay", "shalini", 8, "asdkjfo");
+    }
+
+
 }
